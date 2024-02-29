41ed5595fe9864397ea2e277ed423fe5b5b5685e
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-02-29 12:13:39 -0500

    s4c2 version finale du labo3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Thème wordpress de Salma</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/normalize.scss">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/normalize.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@700&display=swap" rel="stylesheet">
 
