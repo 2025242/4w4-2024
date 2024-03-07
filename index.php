a6c0058f5a412b59cda3d4039e305e7922066a68
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-03-07 10:11:17 -0500

    creation de la branch labo4

--- index.php
+++ index.php
@@ -10,8 +10,15 @@
 
   </head>
   <body>
-    <div id="entete" class="">
-      <header class="entete__header">
+  <div id="menu" class = global>
+        <header class="menu_header">
+            <?php wp_nav_menu(array("container" => "nav")); ?>
+        </header>
+    </div>
+
+<div>
+    <div id="entete" class="global">
+      <section class="entete__header">
         <h1>Thème wordpress de salma (h1)<br>   </h1>
         <h2>4W4 - Concepetion d'interface <span> et dévelopement web</span></h2>
         <h3>TIM-Collège de Maisonneuve</h3><br>
@@ -21,13 +28,9 @@
                 <button>Emploi</button>
                 <button>Événement</button>
      
-      </header>
+      </section>
        
-    <div class="vague">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: var( --couleur-arriere-accueil);"class="shape-fill"></path>
-      </svg>
-    </div>
+    
 
     </div>
     <div id="accueil" class="global">
