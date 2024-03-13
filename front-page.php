996a45636f309c75d95276147493e0a79c1ca765
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-03-13 11:17:29 -0400

    s6c1 depart tp1

---
+++ front-page.php
@@ -0,0 +1,87 @@
+<?php get_header(); ?>
+<h1>front_page</h1>
+<div>
+  <div id="entete" class="global">
+    <section class="entete__header">
+
+      <h2>4W4 - Concepetion d'interface <span> et dévelopement web</span></h2>
+      <h3>TIM-Collège de Maisonneuve <br><br>
+        <button>Nous joindre</button>
+      </h3>
+
+
+    </section>
+    <?php get_template_part("gabarits/vague"); ?>
+
+
+  </div>
+  <div id="accueil" class="global">
+
+    <section>
+      <h2>Acceuil (h2)</h2>
+      <section>
+        <div class="titreCour">Cours</div>
+        <div class="cartes-cours">
+        
+  
+  </section>
+</div>
+<div id="evenement" class="global diagonal">
+  <section>
+    <h3>Évement (h3)</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </section>
+</div>
+<div id="galerie" class="global">
+  <section>
+    <h4>Galerie (h4)</h4>
+    <div class="image">
+      <img src="image/image1.jpg" width="300" height="200">
+      <img src="image/image2.jpg" width="300" height="200">
+      <img src="image/image3.jpg" width="300" height="200">
+    </div>
+
+  </section>
+
+  <div class="vaguefooter">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: var( --couleur-arriere-footer);" class="shape-fill"></path>
+    </svg>
+  </div>
+</div>
+<div id="footer" class="global">
+
+  <footer>
+    <h5>Footer(h5)</h5><br>
+    <div class="footer-content">
+      <div class="footer-section about">
+        <h2>À propos de nous</h2>
+        <p>Elle représente notre idéal, notre aspiration, notre objectif ultime...</p>
+        <!-- Ajoutez d'autres informations à propos de votre entreprise ici -->
+      </div>
+
+      <div class="footer-section contact">
+        <h2>Contactez-nous</h2>
+        <p>Adresse: 3800 R. Sherbrooke E, Montréal, QC H1X 2A2</p>
+        <p>Email: ad.internationale@cmaisonneuve.qc.ca.</p>
+        <p>Téléphone: (514) 254-7131</p>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Liens rapides</h2>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Produits</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="footer-bottom">
+      <p>&copy; 2024 College Maisonneuve. Tous droits réservés.</p>
+    </div>
+    </body>
+
+    </html>
