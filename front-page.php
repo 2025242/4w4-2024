a3ed76d0f752001e68d2d82148e8344e50bde78d
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-04-04 16:38:40 -0400

    s8c2 amelioration des case des categorie et des destination populaire

--- front-page.php
+++ front-page.php
@@ -18,23 +18,57 @@
     <?php get_template_part("gabarits/vague"); ?>
 </div>
     <div id="accueil" class="global">
+      
+       
         <section>
             <h2>Destination Populaire</h2>
-            <div class="destinations">
-            <?php if (have_posts()):
-                while(have_posts()): the_post(); ?>
-                <div class="carte">
-                    <h3><?php the_title(); ?></h3>  
-                    <p><?php echo wp_trim_words(get_the_content(),10); ?> </p>
-                    <?php the_category(); ?>
-                      <!-- ajoute un lien vers l'article   -->
-                       <a href="<?php the_permalink(); ?>" class="lien-suite">Suite</a>
-                </div> 
-                
-               <?php endwhile; ?>
-            <?php endif; ?>
-            </div>
-        </section>
+    <div class="destination">
+        <?php if (have_posts()):
+            while(have_posts()): the_post(); ?>
+                <div class="case">
+                    <div class="nft">
+                        <div class='main'>
+                            <?php
+                        // Récupérer le contenu de l'article
+                        $content = get_the_content();
+
+                        // Utiliser une expression régulière pour extraire l'URL de l'image
+                        preg_match('/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/i', $content, $matches);
+                        
+                        // Vérifier si une image a été trouvée dans le contenu
+                        if (!empty($matches[1])) {
+                            $image_url = $matches[1];
+                            echo '<img class="tokenImage" src="' . $image_url . '" alt="NFT" />';
+                        } else {
+                            // Si aucune image n'a été trouvée dans le contenu, afficher une image par défaut
+                            echo '<img class="tokenImage" src="chemin/vers/image-par-defaut.jpg" alt="NFT" />';
+                        }
+                        ?>
+                            <h2><?php the_title(); ?></h2>
+                            <p class='description'><?php echo wp_trim_words(get_the_content(), 20); ?></p>
+                            <div class='tokenInfo'>
+                                <div class="price">
+                                    <ins>◘</ins>
+                                    <p><?php /* Insérer le prix dynamique */ ?></p>
+                                </div>
+                                <div class="duration">
+                                    <ins> <?php the_category(); ?></ins>
+                                    <p><?php /* Insérer la durée dynamique */ ?></p>
+                                </div>
+                            </div>
+                            <hr />
+                       
+                            <div class='creator'>
+
+                                <p><ins></ins>  <a href="<?php the_permalink(); ?>" class="lien-suite">Suite</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile;
+        endif; ?>
+    </div>
+</section>
     </div>
     <div id="evenement" class="global diagonal">
         <section>
@@ -57,7 +91,7 @@ En définitive, voyager, c'est bien plus que parcourir des kilomètres sur une c
     <div id="galerie" class="global">
         <section>
             <h2>les destination par catégorie</h2>
-            <article class = "destinations">
+            <article class = "destination">
             <?php
             $categories = get_categories();
             foreach ($categories as $elm_categorie){
@@ -77,7 +111,11 @@ En définitive, voyager, c'est bien plus que parcourir des kilomètres sur une c
            <?php }?>
             
             </article>
+              <div class="vague">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: var(--couleur-arriere-footer);"class="shape-fill"></path>
+      </svg>
         </section>
-  <?php  get_template_part('gabarits/vague'); ?>
+ 
     </div>
 <?php get_footer(); ?>
