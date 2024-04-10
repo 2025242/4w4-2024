836799727aef97d2d1304c34f6603ef247988576
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-04-10 10:27:55 -0400

    s9c1 changement screenshot

--- front-page.php
+++ front-page.php
@@ -1,4 +1,3 @@
-
 <?php get_header(); ?>
     
 <div id="entete" class="global">
@@ -27,24 +26,24 @@
             while(have_posts()): the_post(); ?>
                 <div class="case">
                     <div class="nft">
-                        <div class='main'>
+                       <div class='main'>
                             <?php
                         // Récupérer le contenu de l'article
 
                         $content = get_the_content();
 
-                        // Utiliser une expression régulière pour extraire l'URL de l'image
-                        preg_match('/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/i', $content, $matches);
-                        the_post_thumbnail( 'thumbnail' ); 
-                        // Vérifier si une image a été trouvée dans le contenu
-                        if (!empty($matches[1])) {
-                            $image_url = $matches[1];
-                            echo '<img class="tokenImage" src="' . $image_url . '" alt="NFT" />';
-                        } else {
-                            // Si aucune image n'a été trouvée dans le contenu, afficher une image par défaut
-                            echo '<img class="tokenImage" src="chemin/vers/image-par-defaut.jpg" alt="NFT" />';
-                        }
+                      // Récupérer l'URL de l'image mise en avant
+    $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
+
+    // Vérifier si une image mise en avant existe
+    if (!empty($featured_image_url)) {
+        echo '<img class="tokenImage" src="' . esc_url($featured_image_url) . '" alt="NFT" />';
+    } else {
+        // Si aucune image mise en avant n'est définie, afficher une image par défaut
+        echo '<img class="tokenImage" src="chemin/vers/image-par-defaut.jpg" alt="NFT" />';
+    }
                         ?>
+
                             <h2><?php the_title(); ?></h2>
                             <p class='description'><?php echo wp_trim_words(get_the_content(), 20); ?></p>
                             <div class='tokenInfo'>
