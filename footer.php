5e5fd848420fe1e4c750768293974eeab37a5dbf
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-05-09 10:58:05 -0400

    s13c2 finalisation du footer selon le barem du tp2 et le rendre adaptatif

--- footer.php
+++ footer.php
@@ -10,20 +10,46 @@
 
  <footer>
     <div class="footer-content">
+      <div><img src="http://127.0.0.1:8080/4w4salma/wp-content/themes/themesalma/image/logo.png" alt="Logo" class="logoFooter">   </div>
       <div class="footer-section about">
-        <h2>À propos de nous</h2>
-        <p>Auteur: Bourakkadi salma</p>
-          <p>Adresse:3800 R. Sherbrooke E, Montréal, QC H1X 2A2</p>
-          <a href="https://2025242.github.io/4w4-2024/">lien github</a>
+        <h2>À propos</h2>
+        <p class="textealigne">Auteur: Bourakkadi salma</p>
+        <p class="textealigne">Adresse: 3800 R.Sherbrooke E,Montréal,QC H1X2A2</p>
+         
       
       </div>
 
       <div class="footer-section contact">
-        <h2>Contactez-nous</h2>
-        <p>Besoin d'aide pour planifier votre prochain voyage ou avez-vous des questions concernant nos services ? N'hésitez pas à nous contacter. Notre équipe dévouée est là pour vous aider et répondre à toutes vos interrogations.</p><br>
-        <p>Email: [explorerlinconnu@email.com]</p>
-        <p>Téléphone: (514) 254-7131</p>
-          <ul class="social-icon">
+        <h2>Description</h2>
+                   <p class="textealigne">Email: [explorerlinconnu@email.com]</p>
+        <p class="textealigne">Téléphone: (514) 254-7131</p>
+   
+      </div>
+
+      <div class="footer-section links">
+        <h2>Liens rapides</h2>
+         <p class="textealigne"> <a href="https://2025242.github.io/4w4-2024/">Github Page</a></p>
+         <p class="textealigne"> <a href="https://www.cmaisonneuve.qc.ca/">College Maisonneuve</a></p>
+          <p class="textealigne"> <a href="https://github.com/2025242/4w4-2024/tree/tp2">Branche TP2 github</a></p>      
+       <ul>
+
+    </div>
+    </div>  
+
+    <div class="footer-bottom"> 
+         
+      <p>&copy; 2024 College Maisonneuve. Tous droits réservés.</p><br>
+    <div class="conteneurRecherche">
+
+    <div class="search-form">
+        <?php get_search_form(); ?>
+    </div>
+</div>
+
+           
+    </div>
+  
+           <ul class="social-icon">
       <li class="social-icon__item"><a class="social-icon__link" href="#">
           <ion-icon name="logo-facebook"></ion-icon>
         </a></li>
@@ -38,25 +64,10 @@
         </a></li>
     </ul>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-      </div>
-
-      <div class="footer-section links">
-        <h2>Liens rapides</h2>
-         <p>AUTRE</p>
-          <p>//</p><br>
-          <h4 class = "menufooter"><?php wp_nav_menu(array(
+  <h4 class="menufooter"><?php wp_nav_menu(array(
                 "menu"=> "footer",  
-                "container" => "nav")); ?></h4>
-       <ul>
-
-    </div>
-    </div>  
-
-    <div class="footer-bottom">
-      <p>&copy; 2024 College Maisonneuve. Tous droits réservés.</p>
-    </div>
-    
-
+                "container" => "nav")); ?></h4> 
+                
 </footer>
 
 </div>
--- front-page.php
+++ front-page.php
@@ -73,19 +73,17 @@
     <div id="evenement" class="global diagonal">
         <section>
             <h2> Le voyage</h2>
-            <p>Les voyages, ces périples qui nous emportent loin de nos habitudes et nous plongent dans l'inconnu, sont une aventure à part entière. Ils offrent un éventail infini de découvertes, de rencontres et d'expériences qui enrichissent nos vies de manière inestimable.
-
-Partir en voyage, c'est s'ouvrir à de nouveaux horizons, tant géographiques que culturels. C'est explorer des contrées lointaines où chaque paysage raconte une histoire, où chaque ville exhale un parfum unique, où chaque rue résonne des échos du passé et du présent entremêlés.
-
-C'est aussi partir à la rencontre de l'autre, de celui qui vit différemment, qui pense autrement, qui parle une langue étrangère. C'est s'imprégner de sa culture, de ses traditions, de sa cuisine, pour mieux comprendre la diversité du monde qui nous entoure.
-
-Mais voyager, c'est également se confronter à l'imprévu, aux imprévus, aux défis qui jalonnent tout périple. C'est apprendre à s'adapter, à improviser, à sortir de sa zone de confort pour mieux apprécier les moments de joie et surmonter les épreuves avec courage et détermination.
-
-Au-delà de la simple découverte, voyager permet aussi de se découvrir soi-même. C'est l'occasion de se confronter à ses peurs, à ses limites, à ses préjugés, pour mieux les dépasser et s'épanouir en tant qu'individu.
-
-Et puis, il y a ces instants magiques, ces souvenirs impérissables que seul le voyage peut offrir. Le coucher de soleil sur une plage déserte, la rencontre fortuite avec un inconnu qui devient un ami, la saveur exquise d'un plat typique dégusté au détour d'une ruelle étroite... autant de moments qui resteront gravés dans notre mémoire pour l'éternité.
-
-En définitive, voyager, c'est bien plus que parcourir des kilomètres sur une carte. C'est une quête de sens, une exploration de soi et du monde qui nous entoure, une source inépuisable de découvertes et d'émotions. Alors, que ce soit à quelques pas de chez soi ou à l'autre bout de la planète, partons à la rencontre de l'aventure qui nous attend, avec curiosité et enthousiasme. Car comme le dit si bien Saint-Augustin : "Le monde est un livre, et ceux qui ne voyagent pas n'en lisent qu'une page</p>
+            <p> <?php
+                // Appel du shortcode directement dans le fichier front-page.php
+                echo do_shortcode('[em_destination]');
+                ?></p>
+        </section>
+        
+        <section>   
+            <?php
+                // Appel du shortcode directement dans le fichier front-page.php
+                echo do_shortcode('[em_destination]');
+                ?>
         </section>
     </div>
     <div id="galerie" class="global">
