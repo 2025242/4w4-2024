33c1dc94f739b4536179640edbccf20ccc2a170c
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-05-22 12:34:08 -0400

    s15c1 adaptation du menu en mobile et amelioration de laparition du menu burger

--- header.php
+++ header.php
@@ -21,13 +21,12 @@
             </a>
             <h1>Thème WordPress de Salma</h1>
              <div  class = "search-form"><?php get_search_form()?></div>
-            <input class = "checkbox" type="checkbox" name="" id="chk_burger">
-            <label id="burger" for="chk_burger">
-                <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000">
-                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-                </svg>
-            </label>
-           
+           <input class="checkbox" type="checkbox" name="" id="chk_burger">
+    <label id="burger" for="chk_burger">
+        <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000">
+            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+        </svg>
+    </label>
             <?php wp_nav_menu(array(
                 "menu"=> "principal",  
                 "container" => "nav")); ?>
