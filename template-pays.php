e6e8b228d67de517b7a4a02e4404a3ada55e8981
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2024-05-31 19:20:20 -0400

    ajout amelioration css de la section restapi

--- template-pays.php
+++ template-pays.php
@@ -4,16 +4,19 @@ Template Name: Pays
 */
 get_header();
 ?>
+
 <div class="contenu-pays">
-    <?php if ( have_posts() ) : the_post(); ?>
-        <div class="conference__contenu">
-            <h1><?= get_the_title(); ?></h1>
-            <div class="conference__image">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="pays__contenu">
+            <h1 class="pays__titre"><?= get_the_title(); ?></h1>
+            <div class="pays__image">
                 <?php the_post_thumbnail(); ?>
             </div>
-            <div class="conference__description">
+            <div class="pays__description">
                 <?php the_content(); ?>
             </div>
+        </div>
+    <?php endwhile; endif; ?>
     <?php echo do_shortcode('[em_pays]'); ?>
 </div>
 <?php
